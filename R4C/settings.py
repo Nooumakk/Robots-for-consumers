a7a75000801bd4109c2f1ee8998ab8820d837247
Daniel <cskteam4@gmail.com>
2023-09-27 12:51:50 +0300

    Creating API-endpoint for robot model and add validation for it

--- R4C/settings.py
+++ R4C/settings.py
@@ -14,7 +14,6 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -31,7 +30,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -58,9 +57,7 @@ TEMPLATES = [
 WSGI_APPLICATION = "R4C.wsgi.application"
 
 
-
 DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": os.path.join(BASE_DIR, "db.sqlite3")}}
-
 
 
 AUTH_PASSWORD_VALIDATORS = [
@@ -69,7 +66,6 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
-
 
 
 LANGUAGE_CODE = "en-us"
@@ -81,7 +77,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 
 STATIC_URL = "/static/"
